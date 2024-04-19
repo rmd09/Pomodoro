67e750ebd331e34752040f64b5058ec9e7a96d6f
rmd09 <rmd09@yandex.ru>
2024-04-19 18:31:19 +0300

    Чистка кода и дебаг
    Теперь при окончании цикла работы/отдыха аларм стопит выполнение кода. Если кликнуть на кнопку, фларм выключится в любом случае

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ const time = document.getElementById("time");
 const timeShablon = document.getElementById("time-shablon");
 const section1 = document.getElementById("section1");
 
+const audio = new Audio("Alarm.mp3");
+const ONE_ITERATION = 1000;
+
 let isTimerSwitchedOn = false;
-let timeValue = 1500; //Изначальное значение времени в секундах = 25 мин
+let timeValue = 1500; //Изначальное значение времени в секундах (1500) = 25 мин
 let isWork = true; //Состояние таймера (работа/отдых)
+let isThisIterationFirst = true; //Флаг первой итерации
 
 button.addEventListener("click", () => {
+    audio.pause();
+    audio.currentTime = 0;
+
     if (isTimerSwitchedOn) {
-        isTimerSwitchedOn = false;
         SwitchOffTimer();
     }
     else {
-        isTimerSwitchedOn = true;
         SwitchOnTimer();
     }
+    isTimerSwitchedOn = !isTimerSwitchedOn;
 });
 
-//Изменение стиля, таймер включён
 function SwitchOnTimer() {
+    //Изменение стиля, таймер включён
     timeShablon.style.display = "none";
     time.style.display = "block";
 
@@ -29,15 +35,16 @@ function SwitchOnTimer() {
     button.style.border = "1px solid rgba(255, 255, 255, 0.35)";
     button.style.color = "rgba(255, 255, 255, 0.35)";
     button.style.transform = "none";
-    setTimeout(() => {
-        button.textContent = "Стоп"
-        button.disabled = false;
-    }, 500);
-
     mainBlock.style.border = "1px solid #fff";
     section1.style.borderBottom = "1px solid #fff";
     mainBlock.style.transform = "none";
     workCheal.style.color = "#FF4343";
+    
+    setTimeout(() => {      //Кнопка разблокируется только спустя полсекунды
+        button.textContent = "Стоп";
+        button.disabled = false;
+    }, 500);
+    ///////////
 
     Start();
 }
@@ -64,15 +71,86 @@ function SwitchOffTimer() {
 }
 
 function Start() {
+    isThisIterationFirst = true;
+    SwitchToWork();
+}
+async function SwitchToWork() {
+    if (isThisIterationFirst)
+        isThisIterationFirst = false;
+    else 
+        await Alarm();
+
+
+    WorkStyle();
     timeValue = 1500;
     isWork = true;
-    UpdateTextTime()
-    setValue();
+    let interval = setInterval(() => {
+        if (!isTimerSwitchedOn) {
+            SwitchOffTimer();
+            clearInterval(interval);
+        }
+        if (timeValue <= 0) {
+            SwitchToRelax();
+            clearInterval(interval);
+        }
+        else {
+            timeValue--;
+        }
+        UpdateTextTime();
+    }, ONE_ITERATION);
 }
+async function SwitchToRelax() {
+    await Alarm();
+
+    RelaxStyle();
+    timeValue = 300;
+    isWork = false;
+
+    let interval = setInterval(() => {
+        if (!isTimerSwitchedOn) {
+            SwitchOffTimer();
+            clearInterval(interval);
+        }
+        if (timeValue <= 0) {
+            SwitchToWork();
+            clearInterval(interval);
+        }
+        else {
+            timeValue--;
+        }
+        UpdateTextTime();
+    }, ONE_ITERATION);
+}
+//Начинается работа (текст становится красным)
+function WorkStyle() {
+    workCheal.style.color = "#FF4343";
+    workCheal.textContent = "Работа";
+    timeValue = 1500;
+}
+
+//Начинается отдых (текст становится зелёным)
+function RelaxStyle() {
+    workCheal.style.color = "#43FF56";
+    workCheal.textContent = "Отдых";
+    timeValue = 300;
+}
+
+function Alarm() {
+    if (audio) {
+        return new Promise((res) => {
+            audio.play();
+            audio.onended = res;
+        })
+    }
+}
+
+
+
+
 function HelpMethod() {
-    setTimeout(setValue, 985);
+    setTimeout(updataValue, 985);
 }
-const setValue = () => {
+const updataValue = () => {
     if (!isTimerSwitchedOn) return;
 
     UpdateTextTime()
@@ -108,11 +186,5 @@ function UpdateTextTime() {
     }
     else {
         time.textContent = `${Math.floor(timeValue / 60)}:${timeValue % 60}`;
-    }
-}
-function Alarm() {
-    let audio = new Audio("Alarm.mp3");
-    if (audio) {
-        audio.play();
     }
 }
